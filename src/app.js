5fb098f48b0c38624767beef8f255d110c9bc6a3
nickthorpe71 <nickthorpe71@gmail.com>
2020-07-23 17:57:45 -0500

    removed bearer validation

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.use(validateBearerToken);
+// app.use(validateBearerToken);
 
 app.use(foldersRouter);
 app.use(notesRouter);
