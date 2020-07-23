9251b79b6d09f28b0fe1e004e73088bfe689cf04
nickthorpe71 <nickthorpe71@gmail.com>
2020-07-22 21:04:58 -0500

    deployed to heroku

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/noteful2',
+  DB_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful2',
   API_TOKEN: process.env.API_TOKEN
 };
